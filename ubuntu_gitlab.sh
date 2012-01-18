1d321ae203eaf37091149e6db733b84dfc67b1c8
Benjamin Lee <ibank@paran.com>
2012-01-18 21:29:06 +0900

    Update ubuntu_gitlab.sh

--- ubuntu_gitlab.sh
+++ ubuntu_gitlab.sh
@@ -1,7 +1,7 @@
 sudo apt-get install python-dev python-pip redis-server libicu-dev
 sudo pip install pygments
 sudo gem install bundler
-git clone http://github.com/ibank/gitlabhq.git
+git clone http://github.com/gitlabhq/gitlabhq.git
 cd gitlabhq
 bundle install --without development test
 bundle exec rake db:setup RAILS_ENV=production
